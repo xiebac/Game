8cc1fc8da91505ce67fcfeaed9d688922cf8954e
5878641abc <250474451@qq.com>
2021-02-24 13:57:40 +0800

    1

---
+++ XGame/Assets/Scripts/AddressableDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+public class AddressableDemo : MonoBehaviour
+{
+    public Transform parent;
+    public void LocalStatic()
+    {
+        Addressables.InstantiateAsync("Assets/Prefabs/localstatic.prefab",parent);
+    }
+    public void LocalNonStatic()
+    {
+        Addressables.InstantiateAsync("Assets/Prefabs/localnonstatic.prefab", parent);
+    }
+    public void RemoteStatic()
+    {
+        Addressables.InstantiateAsync("Assets/Prefabs/remotestatic.prefab", parent);
+    }
+    public void RemoteNonStatic()
+    {
+        Addressables.InstantiateAsync("Assets/Prefabs/remotenonstatic.prefab", parent);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
