a812346b44a8536738d4c1171bfe0497e25f32e8
5878641abc <250474451@qq.com>
2021-02-24 20:49:42 +0800

    1

--- XGame/Assets/Scripts/AddressableDemo.cs
+++ XGame/Assets/Scripts/AddressableDemo.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
 public class AddressableDemo : MonoBehaviour
 {
     public Transform parent;
+    public List<object> _updateKeys;
     public void LocalStatic()
     {
         Addressables.InstantiateAsync("Assets/Prefabs/localstatic.prefab",parent);
@@ -20,6 +23,74 @@ public class AddressableDemo : MonoBehaviour
     public void RemoteNonStatic()
     {
         Addressables.InstantiateAsync("Assets/Prefabs/remotenonstatic.prefab", parent);
+    }
+    public async void UpdateCatalog()
+    {
+        //开始连接服务器检查更新
+        var handle = Addressables.CheckForCatalogUpdates(false);
+        await handle.Task;
+        Debug.Log("check catalog status " + handle.Status);
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            List<string> catalogs = handle.Result;
+            if (catalogs != null && catalogs.Count > 0)
+            {
+                foreach (var catalog in catalogs)
+                {
+                    Debug.Log("catalog  " + catalog);
+                }
+                Debug.Log("download catalog start ");
+                var updateHandle = Addressables.UpdateCatalogs(catalogs, false);
+                await updateHandle.Task;
+                foreach (var item in updateHandle.Result)
+                {
+                    Debug.Log("catalog result " + item.LocatorId);
+                    foreach (var key in item.Keys)
+                    {
+                        Debug.Log("catalog key " + key);
+                    }
+                    _updateKeys.AddRange(item.Keys);
+                }
+                Debug.Log("download catalog finish " + updateHandle.Status);
+            }
+            else
+            {
+                Debug.Log("dont need update catalogs");
+            }
+        }
+        Addressables.Release(handle);
+    }
+    public IEnumerator DownAssetImpl()
+    {
+        var downloadsize = Addressables.GetDownloadSizeAsync(_updateKeys);
+        yield return downloadsize;
+        Debug.Log("start download size :" + downloadsize.Result);
+
+        if (downloadsize.Result > 0)
+        {
+            var download = Addressables.DownloadDependenciesAsync(_updateKeys, Addressables.MergeMode.Union);
+            yield return download;
+            //await download.Task;
+            Debug.Log("download result type " + download.Result.GetType());
+            foreach (var item in download.Result as List<UnityEngine.ResourceManagement.ResourceProviders.IAssetBundleResource>)
+            {
+                var ab = item.GetAssetBundle();
+                Debug.Log("ab name " + ab.name);
+                foreach (var name in ab.GetAllAssetNames())
+                {
+                    Debug.Log("asset name " + name);
+                }
+            }
+            Addressables.Release(download);
+        }
+        Addressables.Release(downloadsize);
+    }
+    public void ClearCache()
+    { 
+    }
+    public void DownLoad()
+    {
+        StartCoroutine(DownAssetImpl());
     }
     // Start is called before the first frame update
     void Start()
