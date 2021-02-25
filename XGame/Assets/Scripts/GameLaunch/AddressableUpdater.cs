7781b2d500a420c1e314d331c985345604636177
5878641abc <250474451@qq.com>
2021-02-25 15:02:15 +0800

    1

--- XGame/Assets/Scripts/GameLaunch/AddressableUpdater.cs
+++ XGame/Assets/Scripts/GameLaunch/AddressableUpdater.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddressableUpdater : MonoBehaviour
 {
+    public Text status_Text;
+    public Slider update_Slider;
+    float totalTime = 0f;
+    bool needUpdateRes = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalTime = 0f;
+        status_Text.text = "正在检测资源更新...";
     }
-
+    public void StartCheckUpdate()
+    {
+        StartCoroutine(checkUpdate());
+    }
+    IEnumerator checkUpdate()
+    {
+        yield return null;
+    }
     // Update is called once per frame
     void Update()
     {
