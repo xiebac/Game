dd74eb7006f8f8ed434c17e80d3efe0df505b498
5878641abc <250474451@qq.com>
2021-02-27 09:58:08 +0800

    1

--- XGame/Assets/Scripts/FrameWork/Addressables/AddressablesManager.cs
+++ XGame/Assets/Scripts/FrameWork/Addressables/AddressablesManager.cs
@@ -1,8 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 public class AddressablesManager : MonoBehaviour
 {
+    public async void LoadSingleSceneAsync(object key)
+    {
+        var loadHandle = Addressables.LoadSceneAsync(key,LoadSceneMode.Single);
+        await loadHandle.Task;
 
+    }
 }
